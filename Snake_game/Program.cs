b1a9b2634f7c88dbf33c4b17d755d4640d6edfea
IviLevicsany <ivan.levente.robert@csany-zeg.hu>
2025-10-01 09:32:57 +0200

    snakev2

--- Snake_game/Program.cs
+++ Snake_game/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake_game
 {
-    internal class Program
+    class Snake
     {
+        int Height = 20;
+        int Width = 30;
+
+        ConsoleKeyInfo keyinfo = new ConsoleKeyInfo();
+        char key = 'w';
+        public void Palya()
+        {
+            //pálya készítése
+            Console.Clear();
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write(".");
+            }
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Height+2));
+                Console.Write(".");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write(".");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition((Width+2), i);
+                Console.Write(".");
+            }
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyinfo = Console.ReadKey(true);
+                key = keyinfo.KeyChar;
+            } 
+        }
+        }
         static void Main(string[] args)
         {
+            Snake snake = new Snake();
+            snake.Palya();
+            Console.ReadKey();
         }
     }
 }
