059537368e498ae4a81074b19631daf7aaa5fbb4
IviLevicsany <ivan.levente.robert@csany-zeg.hu>
2025-10-08 08:16:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Snake_game
+{
+    class Snake
+    {
+        //pálya méretei
+        int Height = 20;
+        int Width = 30;
+
+        //irány változó
+        ConsoleKeyInfo keyinfo = new ConsoleKeyInfo();
+        char key = 'w';
+        public void Palya()
+        {
+            //pálya készítése
+            Console.Clear();
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write(".");
+            }
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Height + 2));
+                Console.Write(".");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write(".");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition((Width + 2), i);
+                Console.Write(".");
+            }
+        }
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyinfo = Console.ReadKey(true);
+                key = keyinfo.KeyChar;
+            } 
+        }
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+            snake.Palya();
+            Console.ReadKey();
+        }
+    }
+}
